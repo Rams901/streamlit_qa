3616a978839fc089c0d90bf60ae17493459d6289
Ramsis Hammadi <47258547+Rams901@users.noreply.github.com>
2023-06-22 22:22:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def get_pipeline():
     return chain
 
 @st.cache(allow_output_mutation=True)
-def answer_question(question: str, context: str): -> str
+def answer_question(question: str, context: str) -> str:
         llm = ChatOpenAI(
             temperature=0,
             model='gpt-3.5-turbo',
