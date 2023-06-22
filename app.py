804cfabca3f42d25babd9043137d56358023ab7a
ramsis <ramsishammadi@gmail.com>
2023-06-22 17:37:57 +0100

    init

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from typing import Dict
-
+import numpy as np
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import LLMChain
+from langchain import PromptTemplate
+import pandas as pd
+from langchain.vectorstores import FAISS
+import requests
+from typing import List
+from langchain.document_loaders import YoutubeLoader
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chat_models import ChatOpenAI
 import pandas as pd
 import PyPDF2
 import streamlit as st
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-from transformers import AutoTokenizer, BertForQuestionAnswering, pipeline
 
 from io import BytesIO
+from dotenv import load_dotenv
 
-st.title("Question-Answering Webapp")
+load_dotenv()
+st.title("Question-Answering RentGPT")
 
 
 @st.cache(allow_output_mutation=True)
 def extract_text_from_pdfs(pdf_files):
+
     # Create an empty data frame
     df = pd.DataFrame(columns=["file", "text"])
     # Iterate over the PDF files
@@ -27,6 +45,7 @@ def extract_text_from_pdfs(pdf_files):
             num_pages = len(pdf_reader.pages)
             # Initialize a string to store the text from the PDF
             text = ""
+
             # Iterate over all the pages
             for page_num in range(num_pages):
                 # Get the page object
@@ -58,49 +77,79 @@ def remove_short_sentences(df):
     return df
 
 
-@st.cache(allow_output_mutation=True)
-def get_relevant_texts(df, topic):
-    model_embedding = SentenceTransformer("all-MiniLM-L6-v2")
-    model_embedding.save("all-MiniLM-L6-v2")
-    cosine_threshold = 0.3  # set threshold for cosine similarity value
-    queries = topic  # search query
-    results = []
-    for i, document in enumerate(df["sentences"]):
-        sentence_embeddings = model_embedding.encode(document)
-        query_embedding = model_embedding.encode(queries)
-        for j, sentence_embedding in enumerate(sentence_embeddings):
-            distance = cosine_similarity(
-                sentence_embedding.reshape((1, -1)), query_embedding.reshape((1, -1))
-            )[0][0]
-            sentence = df["sentences"].iloc[i][j]
-            results += [(i, sentence, distance)]
-    results = sorted(results, key=lambda x: x[2], reverse=True)
-    del model_embedding
+# @st.cache(allow_output_mutation=True)
+# def get_relevant_texts(df, topic):
+    
+#     model_embedding = SentenceTransformer("all-MiniLM-L6-v2")
+#     model_embedding.save("all-MiniLM-L6-v2")
+#     cosine_threshold = 0.3  # set threshold for cosine similarity value
+#     queries = topic  # search query
+#     results = []
+    
+#     for i, document in enumerate(df["sentences"]):
 
-    texts = []
-    for idx, sentence, distance in results:
-        if distance > cosine_threshold:
-            text = sentence
-            texts.append(text)
-    # turn the list to string
-    context = "".join(texts)
-    return context
+#         sentence_embeddings = model_embedding.encode(document)
+#         query_embedding = model_embedding.encode(queries)
+#         for j, sentence_embedding in enumerate(sentence_embeddings):
+
+#             distance = cosine_similarity(
+#                 sentence_embedding.reshape((1, -1)), query_embedding.reshape((1, -1))
+#             )[0][0]
+
+#             sentence = df["sentences"].iloc[i][j]
+#             results += [(i, sentence, distance)]
+    
+#     results = sorted(results, key=lambda x: x[2], reverse=True)
+#     del model_embedding
+
+#     texts = []
+#     for idx, sentence, distance in results:
+#         if distance > cosine_threshold:
+#             text = sentence
+#             texts.append(text)
+#     # turn the list to string
+
+#     context = "".join(texts)
+#     return context
 
 
 @st.cache(allow_output_mutation=True)
 def get_pipeline():
-    modelname = "deepset/bert-base-cased-squad2"
-    model_qa = BertForQuestionAnswering.from_pretrained(modelname)
-    # model_qa.save_pretrained(modelname)
-    tokenizer = AutoTokenizer.from_pretrained("tokenizer-deepset")
-    # tokenizer.save_pretrained("tokenizer-" + modelname)
-    qa = pipeline("question-answering", model=model_qa, tokenizer=tokenizer)
-    return qa
+    # modelname = "deepset/bert-base-cased-squad2"
+    # model_qa = BertForQuestionAnswering.from_pretrained(modelname)
+    # # model_qa.save_pretrained(modelname)
+    # tokenizer = AutoTokenizer.from_pretrained("tokenizer-deepset")
+    # # tokenizer.save_pretrained("tokenizer-" + modelname)
+    # qa = pipeline("question-answering", model=model_qa, tokenizer=tokenizer)
+
+    llm = ChatOpenAI(
+            temperature=0,
+            model='gpt-3.5-turbo'
+        )
+    prompt = PromptTemplate(
+            input_variables=["question", "docs"],
+            template="""
+            As a consultant, your role is to assist the user in analyzing your mortgage documents and providing advice based on the information the user provides.
+            You will help the user with questions related to your insurance, property tax, trends, and cost rates using the documents you provide.
+            Please share your mortgage documents, including the mortgage agreement, insurance policies, and any other relevant paperwork.
+            Once you have reviewed the documents, you will be able to offer detailed analysis and guidance.
+            Answer the following question: {question}
+            Use the following documents: {docs}
+            Only use the factual information from the documents to answer the question.
+            If you feel like you don't have enough information to answer the question, say "I don't know".
+            """,
+        )
+
+
+    # llm = BardLLM()
+    chain = LLMChain(llm=llm, prompt = prompt)
+
+    return chain
 
 
 def answer_question(pipeline, question: str, context: str) -> Dict:
-    input = {"question": question, "context": context}
-    return pipeline(input)
+    # input = {"question": question, "context": context}
+    return pipeline.run(question = question, docs = context)
 
 
 @st.cache(allow_output_mutation=True)
@@ -108,12 +157,17 @@ def create_context(df):
     # path = "data/"
     # files = Path(path).glob("WHR+22.pdf")
     # df = extract_text_from_pdfs(files)
+    embeddings = HuggingFaceEmbeddings()
+
     df["sentences"] = df["text"].apply(
         lambda long_str: long_str.replace("\n", " ").split(".")
     )
-    df = remove_short_sentences(df)
 
-    context = get_relevant_texts(df, topic)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+    docs = text_splitter.create_documents(list(df['text'].values))
+    db = FAISS.from_documents(docs, embeddings)
+    context = db.similarity_search(question, k=4)
     return context
 
 
@@ -134,7 +188,7 @@ if pdf_files:
         df = extract_text_from_pdfs(pdf_files)
         # context = create_context(df)
         # del df
-    topic = st.text_input("Enter the topic you want to ask here")
+    # topic = st.text_input("Enter the topic you want to ask here")
     question = st.text_input("Enter your questions here...")
 
     if question != "":
@@ -145,4 +199,4 @@ if pdf_files:
             answer = answer_question(qa_pipeline, question, context)
             st.write(answer)
         del qa_pipeline
-        del context
+
